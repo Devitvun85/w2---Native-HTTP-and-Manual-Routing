95b932ec85bae72c9dab8bb4e0661b4380d6a8c9
Devitvun85 <67122420120@srru.ac.th>
2026-05-15 10:18:21 +0700

    Fill Ex-2

--- EX-2/server.js
+++ EX-2/server.js
@@ -20,6 +20,54 @@ const server = http.createServer((req, res) => {
         `);
     }
     // Implement more routes here
+    else if (url === '/about' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end(`
+            <html>
+                <head><title>About</title></head>
+                <body>
+                    <h1>About us:</h1>
+                    <p>At CADT we love node.js</p>
+                </body>
+            </html>
+        `);
+    }
+    else if (url === '/contact-us' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end(`
+            <html>
+                <head><title>Contact</title></head>
+                <body>
+                    <h1>Contact us:</h1>
+                    <p>You can reach us via Email: 4s9lK@example.com</p>
+                </body>
+            </html>
+        `);
+    }
+    else if (url === '/products' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end(`
+            <html>
+                <head><title>Products</title></head>
+                <body>
+                    <h1>Our Products</h1>
+                    <p>Buy one get one free!</p>
+                </body>
+            </html>
+        `);
+    }
+    else if (url === '/projects' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end(`
+            <html>
+                <head><title>Projects</title></head>
+                <body>
+                    <h1>Our Projects</h1>
+                    <p>Here are out awesome projects !</p>
+                </body>
+            </html>
+        `);
+    }
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         return res.end('404 Not Found');
